876435b22b32b596696a86e610ebd51308fd4d49
Dreamfarer <dreamfarer@pm.me>
2025-01-16 12:18:47 +0100

    feat: implement 'csv_write' to write Arma arrays to .csv

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ use std::{fs::OpenOptions, io::Write};
 #[arma]
 fn init() -> Extension {
     Extension::build()
-        .command("csv_write", csv_write)
         .command("csv_init", csv_init)
+        .command("csv_write", csv_write)
         .finish()
 }
 
@@ -20,13 +20,25 @@ pub fn csv_init(file_name: String) -> Result<String, String> {
     Ok(format!("Successfully initialized {}!", &file_name))
 }
 
-pub fn csv_write(file_name: String, data: String) -> Result<String, String> {
-    let mut file = match OpenOptions::new().append(true).open(&file_name) {
+pub fn csv_write(file_name: String, data: arma_rs::Value) -> Result<String, String> {
+    let mut file = match OpenOptions::new().write(true).append(true).open(&file_name) {
         Ok(f) => f,
-        Err(_) => return Err(String::from("File not found!"))
+        Err(_) => return Err(String::from("File not found!")),
     };
-    if let Err(_) = writeln!(file, "{}", &data) {
-        return Err(String::from("Failed to write to the file!"));
+    let data_vec = match data.as_vec() {
+        Some(vec) => vec,
+        None => return Err(String::from("Provided data is not an array!")),
+    };
+    let x_start = data_vec[0].as_f64().unwrap_or(0.0);
+    let y = data_vec[1].as_f64().unwrap_or(0.0);
+    let step_size = data_vec[2].as_f64().unwrap_or(0.0);
+    let heights = &data_vec[3..];
+    for (i, height) in heights.iter().enumerate() {
+        let x = x_start + (i as f64) * step_size;
+        let height_value = height.as_f64().unwrap_or(0.0);
+        if let Err(_) = writeln!(file, "{},{},{}", x, y, height_value) {
+            return Err(String::from("Failed to write data row to the file!"));
+        }
     }
-    Ok(format!("Successfully written to {}!", &file_name))
+    Ok(format!("Successfully processed and written to {}!", &file_name))
 }
